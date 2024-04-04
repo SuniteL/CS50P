7737aeb24e3857c54877095ada7799bf08a7333d
Sunite Lauchande <48389179+SuniteL@users.noreply.github.com>
2024-04-04 15:04:55 +0100

    Update hello.py

--- Week 0 - Functions/hello.py
+++ Week 0 - Functions/hello.py
@@ -1,11 +1,12 @@
 # Ask user for their name?
-name = input("What's your name?")
+name = input("What's your name? ")
 
 # User name print
 print("Hello, " + name)
 
 """
-
 This is also a comment
-
 """
+
+# User name print, the comma adds a space after the "Hello," when printed
+print("Hello,", name)
