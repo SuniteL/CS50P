d96546265ae77eaafe3b42328876c25fb81a76a3
Sunite Lauchande <48389179+SuniteL@users.noreply.github.com>
2024-04-04 16:05:17 +0100

    Update hello.py

--- Week 0 - Functions/hello.py
+++ Week 0 - Functions/hello.py
@@ -10,3 +10,18 @@ This is also a comment
 
 # User name print, the comma adds a space after the "Hello," when printed
 print("Hello,", name)
+
+# with the end=""
+print("Hello, ", end="")
+print(name)
+print("Hello, ", end="???")
+print(name)
+
+# with the sep=""
+print("Hello,", name, sep="!!!")
+
+# \ in the quotes
+print("Hello, \"Friend\"")
+
+# curley brackets
+print(f"hello, {name}")
