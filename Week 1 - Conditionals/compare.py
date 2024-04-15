ea8a6c18ebc1d20036d82701fa19fab427c1a843
Sunite Lauchande <48389179+SuniteL@users.noreply.github.com>
2024-04-15 18:21:39 +0100

    Different ways of doing the same thing
    <,>, != and ==

---
+++ Week 1 - Conditionals/compare.py
@@ -0,0 +1,32 @@
+x = int(input("What do you want to assign to x? "))
+y = int(input("What do you want to assign to y? "))
+"""
+if x < y: #bollean expression
+    print("x is less than y")
+
+elif x > y:
+    print("x is greater than y")
+
+#elif x == y:
+#    print("x is equal to y")
+
+else:
+    print("x is equal to y")
+"""
+"""
+if x <y or x > y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+"""
+"""
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+"""
+
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
