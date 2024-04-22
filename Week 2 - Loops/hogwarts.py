5c5e2cfb39da6bca5ec28eaea844211138800d43
Sunite Lauchande <48389179+SuniteL@users.noreply.github.com>
2024-04-22 21:32:53 +0100

    Update hogwarts.py

--- Week 2 - Loops/hogwarts.py
+++ Week 2 - Loops/hogwarts.py
@@ -1,29 +1,11 @@
-####students = ["Hermoine", "Harry", "Ron", "Draco"]
+students = [
+    {"name": "Hermoine", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
+]
+#square bracker is a list
+#curley braces are dictionaries, with key value pairs
 
-#print(students[0])
-#print(students[1])
-#print(students[2])
-
-##for student in students:
-##    print(student)
-
-###for i in range(len(students)):
-###    print(i + 1, students[i])
-
-####houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
-
-#####students = {
-#####    "Hermoine" : "Gryffindor",
-#####    "Harry" : "Gryffindor",
-#####    "Ron" : "Gryffindor",
-#####    "Draco" : "Slytherin",
-#####}
-#keys and values
-
-####print(students["Hermoine"])
-####print(students["Harry"])
-####print(students["Ron"])
-####print(students["Draco"])
-
-#####for  student in students:
-#####    print(student, students[student], sep", ")
+for student in students:
+    print(student["name"], student["house"], sep=", ")
