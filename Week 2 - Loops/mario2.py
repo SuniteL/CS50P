0955381806c232c632fbbc88f7959ca9758239f4
Sunite Lauchande <48389179+SuniteL@users.noreply.github.com>
2024-11-19 23:52:05 +0000

    Update mario2.py

--- Week 2 - Loops/mario2.py
+++ Week 2 - Loops/mario2.py
@@ -1,13 +1,9 @@
 def main():
     print_square(3)
 
-#for each row in suqare
-def print_square(size):
     
-    # For each brick in row
+def print_square(size):
     for i in range(size):
-
-        # Print brick
         for j in range(size):
             print("#", end="")
         print()
