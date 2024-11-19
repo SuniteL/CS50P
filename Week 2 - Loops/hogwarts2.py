a0a9e2eb7eb3e26ffe9fb7a924493e804cf6fbda
Sunite Lauchande <48389179+SuniteL@users.noreply.github.com>
2024-11-19 22:48:08 +0000

    Update hogwarts2.py

--- Week 2 - Loops/hogwarts2.py
+++ Week 2 - Loops/hogwarts2.py
@@ -1,3 +1,6 @@
 students = ["Hermoine", "Harry", "Ron"]
+houses = ["Gryffindor","Gryffindor","Gryffindor","Slytherin",]
 
-print(students[0])
+
+for i in range(len(students)):
+    print(i + 1, students[i])
